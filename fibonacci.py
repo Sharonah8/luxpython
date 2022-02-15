a35a2995d8545bfafc71da993fabd4cf41e3695f
Sharonah8 <nyamongosharon88@gmail.com>
2022-02-15 09:57:15 +0300

    first commit

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+name = input("Enter the number: ")
+
+print(f'The fibonacci sequence is ')
+
+
+def fib(n):
+
+    a = 0
+    b = 1
+
+    if n == 1:
+        print(a)
+
+    else:
+        print(a)
+        print(b)
+
+        for i in range(2,n):
+            c = a+b
+            a = b
+            b = c
+            print(c)
+        
+
+fib(4)
